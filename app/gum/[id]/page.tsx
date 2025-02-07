5ebef9652a9312ee4f1d6dbdd64c3d613aeaa1eb
Connor Mann <cm@connormann.com>
2025-02-07 15:55:56 -0500

    Fix

--- app/gum/[id]/page.tsx
+++ app/gum/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { notFound } from "next/navigation";
 import db from "@/db";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   const version = await db.query.versions.findFirst({
-    where: (versions, { eq }) => eq(versions.gumId, params.id),
+    where: (versions, { eq }) => eq(versions.gumId, id),
     orderBy: (versions, { desc }) => [desc(versions.id)],
   });
 
