6dfa7a877bb2712aec950cd9bfc2df257f5906a8
colindm <colin.d.miller@icloud.com>
2025-02-14 09:55:22 -0500

    Fixed tailwind styles when not editing page

--- app/gum/[id]/Viewer.tsx
+++ app/gum/[id]/Viewer.tsx
@@ -1,5 +1,14 @@
 "use client";
 
 export default function Viewer({ html }: { html: string }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} className="min-h-screen w-full" />;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+              <script src="https://cdn.tailwindcss.com"></script>
+              ${html}
+            `,
+      }}
+    />
+  );
 }
