0ca5b3be49928e3f471b9d287b91fc374d72170a
colindm <colin.d.miller@icloud.com>
2025-02-19 16:17:08 -0500

    Switched .new to SVG instead of font

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,23 +1,6 @@
 import Logo from "@/app/components/Logo";
 import { ImageResponse } from "next/og";
 
-async function loadGoogleFont(font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?text=${encodeURIComponent(text)}&family=${font}:wght@600`;
-  console.log(url);
-  const css = await (await fetch(url)).text();
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    console.log(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error("failed to load font data");
-}
-
 export async function GET() {
   return new ImageResponse(
     (
@@ -37,14 +20,6 @@ export async function GET() {
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: "Arimo",
-          data: await loadGoogleFont("Arimo", ".new"),
-          style: "normal",
-          weight: 600,
-        },
-      ],
     },
   );
 }
