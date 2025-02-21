ab09e81eaa4fbe52f0127f30002dbd4543e2c936
Connor Mann <cm@connormann.com>
2025-02-21 15:33:54 -0500

    Delete clickhouse/schema.sql

--- clickhouse/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS gum_views (
-    gum_id String,
-    user_id String,
-    timestamp DateTime,
-    ip String,
-    user_agent String
-)
-ENGINE = MergeTree()
-ORDER BY (timestamp, gum_id);
