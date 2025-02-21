d513ccbe520773a5fa0a4d6133cfc0c24d6d164f
Sahil Lavingia <sahil.lavingia@gmail.com>
2025-02-21 11:43:05 -0500

    track gum views using clickhouse

--- clickhouse/schema.sql
+++ clickhouse/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS gum_views (
     gum_id String,
-    user_id Nullable(String),
+    user_id String,
     timestamp DateTime,
-    ip Nullable(String),
-    user_agent Nullable(String)
+    ip String,
+    user_agent String
 )
 ENGINE = MergeTree()
 ORDER BY (timestamp, gum_id);
